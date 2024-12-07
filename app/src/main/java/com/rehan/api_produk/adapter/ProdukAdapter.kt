0147ad3dcfc743c0e3463118163657663913c818
rehanpranama <rehanpranama6@gmail.com>
2024-12-07 09:51:58 +0700

    update all

--- app/src/main/java/com/rehan/api_produk/adapter/ProdukAdapter.kt
+++ app/src/main/java/com/rehan/api_produk/adapter/ProdukAdapter.kt
@@ -1,15 +1,18 @@
 package com.rehan.api_produk.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.rehan.api_produk.DetailProdukActivity
 import com.rehan.api_produk.ModelProduk
 import com.rehan.api_produk.R
 
@@ -18,11 +21,13 @@ class ProdukAdapter(
 ) : ListAdapter<ModelProduk, ProdukAdapter.ProdukViewHolder>(ProdukCallBack) {
 
     class ProdukViewHolder (itemView : View, val onClick : (ModelProduk) -> Unit) :
+
         RecyclerView.ViewHolder(itemView){
             private  val imgProduk : ImageView = itemView.findViewById(R.id.imgProduk)
             private  val title : TextView = itemView.findViewById(R.id.txtTitle)
             private  val brand : TextView = itemView.findViewById(R.id.txtBrand)
             private  val price : TextView = itemView.findViewById(R.id.txtPrice)
+            val cardDetail : CardView = itemView.findViewById(R.id.cardProduk)
 
         //cek produk yang saaat ini
 
@@ -59,6 +64,19 @@ class ProdukAdapter(
     override fun onBindViewHolder(holder: ProdukViewHolder, position: Int) {
         val produk = getItem(position)
         holder.bind(produk)
+
+        //detail
+
+        holder.cardDetail.setOnClickListener{
+            val intent = Intent(holder.itemView.context,DetailProdukActivity::class.java)
+            intent.putExtra("title",produk.title)
+            intent.putExtra("description",produk.description)
+            intent.putExtra("brand",produk.brand)
+            intent.putExtra("price",produk.price)
+            intent.putExtra("thumbnail", produk.thumbnail)
+
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
 }
